2d3fa3975eb89bd44d76a764a62fce25b21023ed
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-12-18 23:31:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ ADMINS = int(environ.get("ADMINS", "6081617163"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://animefrnd44:OEgLPjMjrAKR46kE@cluster0.arcfjym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "joinrequetbot")
 
 # If this is True Then Bot Accept New Join Request 
